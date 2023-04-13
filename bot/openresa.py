e047cc134e369d3e3e45efe1730222e2c2a6483a
CMGConseil <36569113+ActurialCapital@users.noreply.github.com>
2023-04-13 18:24:10 +0200

    Updates

--- bot/openresa.py
+++ bot/openresa.py
@@ -14,6 +14,37 @@ logger = logging.getLogger()
 logging.basicConfig(level=logging.INFO)
 logging.getLogger('apscheduler.executors.default').propagate = False
 
+import chromedriver_autoinstaller
+from pyvirtualdisplay import Display
+display = Display(visible=0, size=(800, 800))  
+display.start()
+
+chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
+                                      # and if it doesn't exist, download it automatically,
+                                      # then add chromedriver to path
+
+chrome_options = webdriver.ChromeOptions()    
+
+# Add your options as needed    
+options = [
+  # Define window size here
+   "--window-size=1200,1200",
+    "--ignore-certificate-errors"
+ 
+    #"--headless",
+    #"--disable-gpu",
+    #"--window-size=1920,1200",
+    #"--ignore-certificate-errors",
+    #"--disable-extensions",
+    #"--no-sandbox",
+    #"--disable-dev-shm-usage",
+    #'--remote-debugging-port=9222'
+]
+
+for option in options:
+    chrome_options.add_argument(option)
+
+
 class BookingBot:
     def __init__(self, Config, driver):
         self.config = Config
@@ -65,7 +96,7 @@ class BookingBot:
 
 def main(Config, club_name, date, court_id):
     # Launch the Chrome browser
-    with webdriver.Chrome() as driver:
+    with webdriver.Chrome(options = chrome_options) as driver:
         try:
             # Initialize bot
             self = BookingBot(Config, driver)
