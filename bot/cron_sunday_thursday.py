5790462e50eea923cd11c4fc492e26225b845a77
CMGConseil <36569113+ActurialCapital@users.noreply.github.com>
2023-04-19 16:52:04 +0200

    Updates

---
+++ bot/cron_sunday_thursday.py
@@ -0,0 +1,17 @@
+from datetime import datetime, timedelta
+from openresa import main
+
+if __name__ == "__main__":
+    
+    # params
+    tomorrow = datetime.today() + timedelta(days=1)
+    club_name = "les-pyramides"
+    date = tomorrow.strftime("%d/%m/%Y")
+    slot = '14:00-15:30'
+    court_id = '48097'
+    hour = 17
+    minute = 0
+    second = 0 
+    timezone = 'Europe/Paris'
+
+    main(club_name, date, court_id, slot, hour, minute, second, timezone)
